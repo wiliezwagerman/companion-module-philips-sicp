cbadfb4756e49bbd2747af3ffd85b49a2feb761e
wiliezwagerman <wiliezwagerman@gmail.com>
2024-05-15 20:46:08 +0200

    Module starting

--- src/sicp.ts
+++ src/sicp.ts
@@ -1,10 +1,10 @@
+import { TCPHelper } from '@companion-module/base'
 import { PhilipsSICPConfig } from './config.js'
 
 export class SICPClass {
 	requestSICPVersion: Uint8Array = new Uint8Array([0x06, 0x01, 0x00, 0xa2, 0x00, 0xa5])
 	TurnOffCommand: Uint8Array = new Uint8Array([0x06, 0x01, 0x00, 0x18, 0x01, 0x1e])
-	net = require('node:net')
-	socket = new this.net.Socket()
+	socket: TCPHelper
 
 	#config: PhilipsSICPConfig = {
 		host: '',
@@ -12,9 +12,8 @@ export class SICPClass {
 	}
 
 	constructor(config: PhilipsSICPConfig) {
-		this.socket.setKeepAlive(true)
 		this.#config = config
-		this.socket.connect(this.#config.host, this.#config.port)
+		this.socket = new TCPHelper(this.#config.host, this.#config.port)
 	}
 
 	updateConfig(config: PhilipsSICPConfig): void {
@@ -26,6 +25,7 @@ export class SICPClass {
 	}
 
 	sendCommand(SICPrequest: Uint8Array): void {
-		this.socket.write(SICPrequest, 'hex')
+		const buffer = Buffer.from(SICPrequest)
+		void this.socket.send(buffer)
 	}
 }
